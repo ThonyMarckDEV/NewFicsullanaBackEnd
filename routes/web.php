d4e0541305d2dd583bf2819b9e19b29213417e35
ThonyMarckDEV <thonymarck385213xd@gmail.com>
2025-10-06 10:51:28 -0500

    Corrected

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/reset-password/{idUsuario}/{token}', [PasswordResetController::class, 'showResetForm'])->name('password.reset.form');
-Route::post('/reset-password/{idUsuario}/{token}', [PasswordResetController::class, 'reset'])->name('password.reset.submit');
+Route::get('/reset-password/{id_Usuario}/{token}', [PasswordResetController::class, 'showResetForm'])->name('password.reset.form');
+Route::post('/reset-password/{id_Usuario}/{token}', [PasswordResetController::class, 'reset'])->name('password.reset.submit');
